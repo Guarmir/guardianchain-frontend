e665a3de9c72096ad819c09e7ed694efd3bfce8a
Guarmir <miranda38oficial@gmail.com>
2026-03-07 18:41:34 -0300

    fix language detection

--- src/i18n.js
+++ src/i18n.js
@@ -4,20 +4,26 @@ import { initReactI18next } from "react-i18next"
 import en from "./locales/en.json"
 import pt from "./locales/pt.json"
 
+// detectar idioma do navegador
+const browserLang = navigator.language.startsWith("pt") ? "pt" : "en"
+
 i18n
   .use(initReactI18next)
   .init({
+
     resources: {
       en: { translation: en },
       pt: { translation: pt }
     },
 
-    lng: "en",
+    lng: browserLang,
+
     fallbackLng: "en",
 
     interpolation: {
       escapeValue: false
     }
+
   })
 
 export default i18n
